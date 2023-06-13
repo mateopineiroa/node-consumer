ab846c0c19b5cd826a14624041935a0b03bef3f2
Mateo Piñeiro <mateopineiro2001@gmail.com>
2023-06-13 17:19:10 -0300

    Add env vars

--- app/user/chat/page.tsx
+++ app/user/chat/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { io } from "socket.io-client";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState<
-    { message: string; date: Date }[]
+    { message: String; date: Date; user: String }[]
   >([]);
   const {
     isLoading,
@@ -16,12 +16,15 @@ const Chat = () => {
     if (typeof window !== "undefined") {
       const bearerToken = localStorage.getItem("token");
       try {
-        const res = await fetch("http://localhost:3000/mongoose/user", {
-          headers: {
-            Authorization: `Bearer ${bearerToken}`,
-            "Content-Type": "application/json",
-          },
-        });
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_URL}/mongoose/user`,
+          {
+            headers: {
+              Authorization: `Bearer ${bearerToken}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
         const data = res.json();
 
@@ -35,34 +38,36 @@ const Chat = () => {
     }
   });
 
-  const {
-    isLoading: isLoadingMessages,
-    error: errorMessages,
-    data: messages,
-  } = useQuery("messages", async () => {
-    if (typeof window !== "undefined") {
-      const bearerToken = localStorage.getItem("token");
-      try {
-        const res = await fetch("http://localhost:3000/mongoose/messages", {
-          headers: {
-            Authorization: `Bearer ${bearerToken}`,
-            "Content-Type": "application/json",
-          },
-        });
+  useEffect(() => {
+    const fetchChat = async () => {
+      if (typeof window !== "undefined") {
+        const bearerToken = localStorage.getItem("token");
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_URL}/mongoose/messages`,
+            {
+              headers: {
+                Authorization: `Bearer ${bearerToken}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
 
-        const data = await res.json();
+          const data = await res.json();
 
-        return data;
-      } catch (err) {
-        console.log(err);
-        throw err;
+          setMessageList(data);
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
+      } else {
+        throw "window === undefined";
       }
-    } else {
-      throw "window === undefined";
-    }
-  });
-  console.log(messages);
-  var socket = io("http://localhost:3000");
+    };
+    fetchChat();
+  }, []);
+  // console.log(messages);
+  var socket = io(`${process.env.NEXT_PUBLIC_URL}`);
 
   const handleSubmit = (
     e: FormEvent<HTMLButtonElement> | FormEvent<HTMLFormElement>
@@ -90,25 +95,23 @@ const Chat = () => {
         </button>
       </form>
 
-      <div className="h-full w-[20rem] m-4 bg-white text-black">
-        {!isLoadingMessages &&
-          !errorMessages &&
-          messages.map((msg: { message: string; date: Date; user: string }) => {
-            const msgDate = new Date(msg.date);
-            return (
-              <div className="flex justify-between" key={msg.date.toString()}>
-                <div>
-                  <p>Message: {msg.message}</p>
-                  <p>
-                    {msgDate.getDate()}/{msgDate.getMonth()}/
-                    {msgDate.getFullYear()}, {msgDate.getHours()}:
-                    {msgDate.getMinutes()}:{msgDate.getSeconds()}
-                  </p>
-                </div>
-                <p>User: {msg.user}</p>
+      <div className="flex flex-col-reverse h-full w-[20rem] m-4 bg-white text-black">
+        {messageList.map((msg) => {
+          const msgDate = new Date(msg.date);
+          return (
+            <div className="flex justify-between" key={msg.date.toString()}>
+              <div>
+                <p>Message: {msg.message}</p>
+                <p>
+                  {msgDate.getDate()}/{msgDate.getMonth()}/
+                  {msgDate.getFullYear()}, {msgDate.getHours()}:
+                  {msgDate.getMinutes()}:{msgDate.getSeconds()}
+                </p>
               </div>
-            );
-          })}
+              <p>User: {msg.user}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
